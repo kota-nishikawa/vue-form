8d51de0cca747af3d77862fb886cb71bc01ac921
Kota Nishikawa <kota.nishikawa@site4d.com>
2020-04-22 16:08:02 +0900

    routerコミットと不要な部分の削除

--- src/main.js
+++ src/main.js
@@ -1,40 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-import moment from 'moment'
+import router from './router'
 
 Vue.config.productionTip = false
 
 new Vue({
-
-    el: '#app',
-    data: {
-        months: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-        year: moment().year(),
-        month: moment().month() + 1,
-        date: moment().date()
-    },
-    methods: {
-        getYears: function () {
-            const goBackYears = 10;
-            const currentYear = moment().year();
-            const startYear = currentYear - goBackYears;
-            return [...Array(goBackYears + 1).keys()].map(x => x + startYear);
-        },
-        getDates: function (year, month) {
-            const maxDate = this.getFinalDate(year, month);
-            return [...Array(maxDate).keys()].map(x => x + 1);
-        },
-        modify: function () {
-            // 年や月が変更されたとき、日が存在しなくなる場合があるので調整する。
-            // 例: 2018-12-31 を選択していて月が 12 から 2 に変更された場合、日を 28 にする。
-            if (!moment([this.year, this.month - 1, this.date]).isValid()) {
-                this.date = this.getFinalDate(this.year, this.month);
-            }
-        },
-        getFinalDate: function (year, month) {
-            // 月末日
-            return moment([year, month - 1]).endOf('month').date();
-        }
-    }
-}).$mount()
+  render: h => h(App),
+}).$mount('#app')
